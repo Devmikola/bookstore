917ba2c66cd2eac3d704dc32212cd7a13ae2ac08
Nikolay <devmikola@gmail.com>
2016-05-02 19:46:02 +0300

    Composer updated

--- assets/LightBoxAsset.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\assets;
-use yii\web\AssetBundle;
-
-class LightBoxAsset extends AssetBundle {
-    public $sourcePath = '@bower/lightbox2/src';
-    public $css = ['css/lightbox.css'];
-    public $js = ['js/lightbox.js'];
-}
